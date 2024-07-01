5c60b09915ddd99508a0b03f711df331f6086bb0
Dima Andreyuk <dm.andreyuk@gmail.com>
2024-07-02 00:45:46 +0200

    Added content for about page

--- app/about/page.js
+++ app/about/page.js
@@ -1,7 +1,18 @@
 export const metadata = {
-  title: "About",
+    title: "About",
 };
 
 export default function Page() {
-  return <h1>About The Wild Oasis</h1>;
+    return <>
+        <h1>About The Wild Oasis</h1>
+        <p>
+            Since 1962, The Wild Oasis has stood as a beloved family-owned sanctuary. Founded by our grandparents, this
+            haven has flourished through generations, reflecting our family's unwavering commitment to fostering a warm
+            and inviting atmosphere. Each passing year has seen The Wild Oasis lovingly maintained and cherished, a
+            living testament to our dedication to hospitality and care.
+        </p>
+        <a href="/cabins">
+            Checkout our luxury cabins
+        </a>
+    </>;
 }
