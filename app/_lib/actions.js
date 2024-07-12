5d0308d2f257890e73cdc7be03d77d3edacec983
Raman Andreyuk <romaandreyuk@gmail.com>
2024-07-13 01:26:31 +0200

    Added Edit Reservarion Action

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -3,7 +3,8 @@
 import { revalidatePath } from "next/cache";
 import { auth, signIn, signOut } from "./auth";
 import { supabase } from "./supabase";
-import { getBookings } from "./data-service";
+import { getBookings, updateBooking } from "./data-service";
+import { redirect } from "next/navigation";
 
 export async function updateGuest(formData) {
   const session = await auth();
@@ -51,4 +52,24 @@ export async function signInAction() {
 
 export async function signOutAction() {
   await signOut({ redirectTo: "/" });
+}
+
+export async function updateReservation(formData) {
+  const session = await auth();
+  if (!session) throw new Error("You must be logged in");
+
+  const numGuests = formData.get("numGuests");
+  const observations = formData.get("observations");
+
+  // TODO: add validation
+  const updateData = { numGuests, observations };
+
+  const update = await updateBooking(
+    Number(formData.get("bookingId")),
+    updateData
+  );
+
+  revalidatePath("/account/reservations");
+  revalidatePath(`/account/reservations/edit/${formData.get("bookingId")}`);
+  redirect("/account/reservations");
 }
