8946fbeb3b0836bfafe65ae9d8ae6d6b7198808a
Raman Andreyuk <romaandreyuk@gmail.com>
2025-08-31 00:07:01 +0200

    changed supabase project

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "rznhfhojldmnudrgtowi.supabase.co",
+        hostname: "rbupsxdvjbadgeziqpon.supabase.co",
         port: "",
         pathname: "/storage/v1/object/public/cabin-images/**",
       },
